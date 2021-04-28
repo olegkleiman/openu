0c2935a5a9795a79308bee98c791b044c7d0c981
Oleg Kleiman <oleg_kleyman@hotmail.com>
2021-04-28 11:37:45 +0300

    Update Q3.java

--- Q3.java
+++ Q3.java
@@ -2,26 +2,24 @@ package com.company;
 
 import java.util.Arrays;
 
-public class Main {
-
-    public static void sum(int[] a) {
-        sum(a, 0);
-    }
-
-    public static int sum(int[] a, int index) {
-        if( index == a.length -1 ) // last element?
-            return a[index];
-
-        int _sum = sum(a, ++index);
-        return a[index-1] += _sum;
-    }
-
-
-    public static void main(String[] args) {
-
-        int[] a = new int[]{10,2,3};
-        System.out.println("Before: " + Arrays.toString(a));
-        sum(a);
-        System.out.println("After: " + Arrays.toString(a));
-    }
+public static void sum(int[] a) {
+    sum(a, 0);
 }
+
+public static int sum(int[] a, int index) {
+    if( index == a.length -1 ) // last element?
+        return a[index];
+
+    int _sum = sum(a, ++index);
+    return a[index-1] += _sum;
+}
+
+
+public static void main(String[] args) {
+
+    int[] a = new int[]{10,2,3};
+    System.out.println("Before: " + Arrays.toString(a));
+    sum(a);
+    System.out.println("After: " + Arrays.toString(a));
+}
+
